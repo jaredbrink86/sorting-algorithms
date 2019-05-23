c35c8f1153ea6b8c1f1ed0f78b3329a55078a9a1
Jared Brink <J-radical@Jareds-MacBook-Pro.local>
2019-05-23 13:35:45 -0600

    Added test suite, implemented binarySearch

--- sort.js
+++ sort.js
@@ -1,29 +1,102 @@
-var sameFrequency = function(num1, num2) {
+const sameFrequency = function(num1, num2) {
   // convert both to string
-  var str1 = num1.toString();
-  var str2 = num2.toString();
-  // if numbers are different length return false
+  const str1 = num1.toString();
+  const str2 = num2.toString();
   if (str1.length !== str2.length) {
     return false;
   }
   // create counter object for each string
-  var counter1 = {};
-  var counter2 = {};
+  const counter1 = {};
+  const counter2 = {};
   // loop over first string
-  for (var i = 0; i < str1.length; i++) {
+  for (let i = 0; i < str1.length; i++) {
     counter1[str1[i]] ? counter1[str1[i]]++ : (counter1[str1[i]] = 1);
   }
   // repeat for second string
-  for (var i = 0; i < str2.length; i++) {
+  for (let i = 0; i < str2.length; i++) {
     counter2[str2[i]] ? counter2[str2[i]]++ : (counter2[str2[i]] = 1);
   }
   // loop over one object
-  for (var key in counter1) {
+  for (let key in counter1) {
     // if object[key] in second object doesn't exist, return false or, if object[key] in second object is not the same return false;
-    if (!counter2[key] || counter2[key] !== counter2[key]) {
+    if (!counter2[key] || counter2[key] !== counter1[key]) {
       return false;
     }
   }
   //return true if all loops executed
   return true;
+};
+
+const areThereDuplicates = function() {
+  let args = Array.from(arguments).sort();
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] === args[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+var isSubsequence = function(str1, str2) {
+  // set index of str1 to 0
+  var j = 0;
+  // loop through string 2
+  for (var i = 0; i < str2.length; i++) {
+    if (i === str2.length - 1 && j < str1.length - 1) {
+      // return false
+      return false;
+    }
+    // if string 1 index matches string 2 index
+    if (str1[j] === str2[i]) {
+      // incrememnt both indexes
+      j++;
+    }
+  }
+  // return true if made it through the loop
+  return true;
+};
+
+const maxSubarraySum = function(array, n) {
+  // if n is greater than array.length
+  if (n > array.length) {
+    return null;
+  }
+  // set max variable at 0
+  let maxSum = 0;
+  // set temp variable at 0
+  let tempSum = 0;
+  // loop up to n
+  for (let i = 0; i < n; i++) {
+    // add each element value to maxSum
+    maxSum += array[i];
+  }
+  tempSum = maxSum;
+  // loop through array starting and index n
+  for (let i = n; i < array.length; i++) {
+    // subtract from element at i - n and add element at i and store in tempSum variable
+    tempSum = tempSum - array[i - n] + array[i];
+    // if tempSum is greater than maxSum
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  //return max
+  return maxSum;
+};
+
+const binarySearch = function(arr, elem) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === elem ? middle : -1;
+};
+module.exports = {
+  sameFrequency,
+  areThereDuplicates,
+  isSubsequence,
+  maxSubarraySum,
+  binarySearch
 };
---
+++ sort.test.js
@@ -0,0 +1,67 @@
+const {
+  sameFrequency,
+  areThereDuplicates,
+  isSubsequence,
+  maxSubarraySum,
+  binarySearch
+} = require("./sort.js");
+
+describe("sameFrequency", () => {
+  test("Should return a boolean", () => {
+    expect(typeof sameFrequency(123, 321)).toBe("boolean");
+  });
+
+  test("Should return true if both numbers have the same numbers, occurring with the same frequency", () => {
+    expect(sameFrequency(123, 321)).toBe(true);
+    expect(sameFrequency(123, 123)).toBe(true);
+    expect(sameFrequency(1114445, 4141415)).toBe(true);
+  });
+
+  test("Should return false if contains different numbers, or same numbers occur with different same frequency", () => {
+    expect(sameFrequency(123, 221)).toBe(false);
+    expect(sameFrequency(244, 221)).toBe(false);
+    expect(sameFrequency(414, 411)).toBe(false);
+  });
+});
+
+describe("areThereDuplicates", () => {
+  test("Should return a boolean", () => {
+    expect(typeof areThereDuplicates(1, 2, 3, 4, 4)).toBe("boolean");
+  });
+  test("Should return true if duplicates are found", () => {
+    expect(areThereDuplicates(1, 1, 2, 4)).toBe(true);
+    expect(areThereDuplicates("a", "b", "c", "a", "c")).toBe(true);
+  });
+  test("Should return false if duplicates are not found", () => {
+    expect(areThereDuplicates(1, 2, 3, 4, 5)).toBe(false);
+    expect(areThereDuplicates("a", "b", "c", "d", "e")).toBe(false);
+  });
+});
+
+describe("isSubsequence", () => {
+  test("Should return a boolean", () => {
+    expect(typeof isSubsequence("abc", "bc")).toBe("boolean");
+  });
+  test("Should return true if one string is a substring of the other", () => {
+    expect(isSubsequence("hello", "hello world")).toBe(true);
+  });
+  test("Should return false if one string is a substring of the other", () => {
+    expect(isSubsequence("hello", "goodbye")).toBe(false);
+  });
+});
+
+describe("maxSubarraySum", () => {
+  test("Should return the largest total of n consecutive numberes in the array", () => {
+    expect(maxSubarraySum([1, 2, 3, 4, 5], 2)).toBe(9);
+    expect(maxSubarraySum([1, 2, 5, 4, 2, 1, 4, 9, 8], 3)).toBe(21);
+  });
+});
+
+describe("binarySearch", () => {
+  test("Should return the index of the given element, if included in the array", () => {
+    expect(binarySearch([1, 2, 3, 4], 3)).toBe(2);
+  });
+  test("Should return -1 if the element is not in the array", () => {
+    expect(binarySearch([1, 2, 3, 4], 5)).toBe(-1);
+  });
+});
